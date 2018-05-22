381c4961ee8f4f086854a56dbab3eccce6270737
Matthew Krohn <matthewkrohn@7400s-MacBook-Pro.local>
2018-05-22 15:34:27 -0600

    modified:   dispMenu.sh Services stop and start without reboot

--- dispMenu.sh
+++ dispMenu.sh
@@ -6,35 +6,47 @@ vi_editor=${EDITOR-vi}
 
 use_dev_cropped ()
 {
+	sudo service fbcp stop
+	sudo service fbcpCropped stop
+	sudo service fbcpFilled stop
+
 	sudo update-rc.d fbcpCropped.sh enable
 	sudo update-rc.d fbcpFilled.sh disable
 	sudo update-rc.d fbcp.sh disable
+
+	sudo service fbcpCropped start
 	echo "Using Cropped Experimental Driver"
-	echo "System will Shut Down in 10 Seconds"
-	sleep 10
-	sudo reboot
+	sleep 5
 }
 
 use_dev_filled ()
 {
+	sudo service fbcp stop
+	sudo service fbcpCropped stop
+	sudo service fbcpFilled stop
+
 	sudo update-rc.d fbcpCropped.sh disable
 	sudo update-rc.d fbcpFilled.sh enable
 	sudo update-rc.d fbcp.sh disable
+
+	sudo service fbcpFilled start
 	echo "Using Filled Experimental Driver"
-	echo "System will Shut Down in 10 Seconds"
-	sleep 10
-	sudo reboot
+	sleep 5
 }
 
 use_std ()
 {
+	sudo service fbcp stop
+	sudo service fbcpCropped stop
+	sudo service fbcpFilled stop
+
 	sudo update-rc.d fbcpCropped.sh disable
 	sudo update-rc.d fbcpFilled.sh disable
 	sudo update-rc.d fbcp.sh enable
+
+	sudo service fbcp start
 	echo "Using Standard Driver"
-	echo "System will Shut Down in 10 Seconds"
-	sleep 10
-	sudo reboot
+	sleep 5
 }
 
 dialog --clear --title "LCD Driver Selection" \
