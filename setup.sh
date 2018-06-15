db8a670be5cfe096237f7f2b9c80fd209a140b74
Matthew Krohn <matthew.d.krohn@gmail.com>
2018-06-15 10:30:47 -0600

    Trying old verison of setup with dtoverlay and hdmi mode change

--- setup.sh
+++ setup.sh
@@ -20,4 +20,6 @@ sudo update-rc.d fbcpFilled.sh defaults
 sudo update-rc.d fbcpCropped.sh defaults
 sudo update-rc.d fbcpZero.sh defaults
 
+sudo sed -i 's|dtoverlay=waveshare32b,speed=80000000,fps=60,rotate=270|#dtoverlay=waveshare32b,speed=80000000,fps=60,rotate=270|' /boot/config.txt
+sudo sed -i 's|hdmi_mode=16|hdmi_mode=4|' /boot/config.txt
 sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./dispMenu.sh</path>\n\t\t<name>Freeplay Change Display Driver</name>\n\t\t<desc>Choose which display driver to use, between two versions of the experimental driver and the default driver</desc>\n\t\t<image>./icons/display.png</image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
