71fa6268806a62fcb75cd7d8a973dc85943ed0f1
Matthew Krohn <matthew.d.krohn@gmail.com>
2018-06-18 12:56:40 -0600

    Readability changes

--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-sudo cp fbcpFilled /usr/local/bin/fbcpFilled
-sudo cp fbcpCropped /usr/local/bin/fbcpCropped
-sudo cp dispMenu.sh /home/pi/RetroPie/retropiemenu/dispMenu.sh
-sudo cp display.png /home/pi/RetroPie/retropiemenu/icons/display.png
-sudo cp fbcpCropped.sh /etc/init.d/fbcpCropped.sh
-sudo cp fbcpFilled.sh /etc/init.d/fbcpFilled.sh
-sudo cp fbcpZero.sh /etc/init.d/fbcpZero.sh
-sudo cp fbcpZero /usr/local/bin/fbcpZero
+sudo cp fbcpFilled /usr/local/bin
+sudo cp fbcpCropped /usr/local/bin
+sudo cp fbcpZero /usr/local/bin
+sudo cp dispMenu.sh /home/pi/RetroPie/retropiemenu
+sudo cp display.png /home/pi/RetroPie/retropiemenu/icons
+sudo cp fbcpCropped.sh /etc/init.d
+sudo cp fbcpFilled.sh /etc/init.d
+sudo cp fbcpZero.sh /etc/init.d
 
 sudo chmod +x /etc/init.d/fbcpCropped.sh
 sudo chmod +x /etc/init.d/fbcpFilled.sh
 sudo chmod +x /etc/init.d/fbcpZero.sh
+
 sudo chown root:root /etc/init.d/fbcpCropped.sh 
 sudo chown root:root /etc/init.d/fbcpFilled.sh
 sudo chown root:root /etc/init.d/fbcpZero.sh
@@ -23,5 +24,6 @@ sudo update-rc.d fbcpZero.sh defaults
 sudo update-rc.d fbcpFilled.sh disable
 sudo update-rc.d fbcpCropped.sh disable
 sudo update-rc.d fbcpZero.sh disable
+sudo update-rc.d fbcp.sh enable
 
 sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./dispMenu.sh</path>\n\t\t<name>Freeplay Change Display Driver</name>\n\t\t<desc>Choose which display driver to use, between two versions of the experimental driver and the default driver</desc>\n\t\t<image>./icons/display.png</image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
