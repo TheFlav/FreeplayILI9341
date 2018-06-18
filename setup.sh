3277018593217fe187329cfed27a0b62dede1cee
Matthew Krohn <matthew.d.krohn@gmail.com>
2018-06-18 12:30:29 -0600

    trying some fixes

--- setup.sh
+++ setup.sh
@@ -20,4 +20,8 @@ sudo update-rc.d fbcpFilled.sh defaults
 sudo update-rc.d fbcpCropped.sh defaults
 sudo update-rc.d fbcpZero.sh defaults
 
+sudo update-rc.d fbcpFilled.sh disable
+sudo update-rc.d fbcpCropped.sh disable
+sudo update-rc.d fbcpZero.sh disable
+
 sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./dispMenu.sh</path>\n\t\t<name>Freeplay Change Display Driver</name>\n\t\t<desc>Choose which display driver to use, between two versions of the experimental driver and the default driver</desc>\n\t\t<image>./icons/display.png</image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
