4951820bc6fad4e5258c136f1f40ef4e4f8f4b9f
Matthew Krohn <matthew.d.krohn@gmail.com>
2018-09-18 21:25:49 +0000

    Added new No DMA version, but CPU usage suffers greatly

--- dispMenuZero.sh
+++ dispMenuZero.sh
@@ -9,6 +9,7 @@ update ()
     git -C /home/pi/Freeplay/freeplayili9341 pull
 
     sudo cp fbcpZero /usr/local/bin
+    sudo cp fbcpZeroNoDMA /usr/local/bin
     sudo cp dispMenuZero.sh /home/pi/RetroPie/retropiemenu/dispMenu.sh
 }
 
@@ -18,16 +19,42 @@ use_zero ()
     
     sudo service fbcp stop
     sudo systemctl stop fbcpZero.service
+    sudo systemctl stop fbcpZeroNoDMA.service
     sudo killall Freeplay-fbcp
     sudo killall fbcpZero
+    sudo killall fbcpZeroNoDMA
     
     sleep 1
     
     sudo update-rc.d fbcp.sh disable
+    sudo systemctl disable fbcpZeroNoDMA.service
     sudo systemctl enable fbcpZero.service
     
     sleep 1
     sudo systemctl start fbcpZero.service
+    sleep 2
+    sudo reboot
+}
+
+use_zero_no_dma ()
+{
+    sudo sed -i 's|^dtoverlay=waveshare32b|#FP#dtoverlay=waveshare32b|' /boot/config.txt
+    
+    sudo service fbcp stop
+    sudo systemctl stop fbcpZero.service
+    sudo systemctl stop fbcpZeroNoDMA.service
+    sudo killall Freeplay-fbcp
+    sudo killall fbcpZero
+    sudo killall fbcpZeroNoDMA
+    
+    sleep 1
+    
+    sudo update-rc.d fbcp.sh disable
+    sudo systemctl enable fbcpZeroNoDMA.service
+    sudo systemctl disable fbcpZero.service
+    
+    sleep 1
+    sudo systemctl start fbcpZeroNoDMA.service
     sleep 2
     sudo reboot
 }
@@ -38,12 +65,16 @@ use_std ()
     
     sudo service fbcp stop
     sudo systemctl stop fbcpZero.service
+    sudo systemctl stop fbcpZeroNoDMA.service
+    sudo killall Freeplay-fbcp
     sudo killall fbcpZero
+    sudo killall fbcpZeroNoDMA
     
     sleep 1
     
     sudo update-rc.d fbcp.sh enable
     sudo systemctl disable fbcpZero.service
+    sudo systemctl disable fbcpZeroNoDMA.service
     
     sleep 1
     sudo service fbcp start
@@ -55,6 +86,7 @@ dialog --clear --title "LCD Driver Selection" \
 --menu "Choose which LCD Driver you would like to use" 15 50 4 \
 Default "Default Driver" \
 Exp_Zero "Exp Cropped for FP Zero" \
+NoDMA_Zero "FP Zero w/out DMA" \
 Update "Update binaries and Menu" \
 Exit "Exit without any changes" 2>"${INPUT}"
 
@@ -63,6 +95,7 @@ menuitem=$(<"${INPUT}")
 case "$menuitem" in
     Default) use_std;;
     Exp_Zero) use_zero;;
+    NoDMA_Zero) use_zero_no_dma;;
     Update) update;;
     Exit) echo "No changes made"; break;;
 esac
