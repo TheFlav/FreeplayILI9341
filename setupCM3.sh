3fb413affc41c33e7ff91effe326bc9d923398dd
Matthew Krohn <matthew.d.krohn@gmail.com>
2019-05-28 14:21:18 -0600

    Updated install scripts to correctly check for preinstalled menu item, better directory checking

--- setupCM3.sh
+++ setupCM3.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-sudo cp fbcpFilled /usr/local/bin/fbcpFilled
-sudo cp fbcpCropped /usr/local/bin/fbcpCropped
-sudo rm /home/pi/RetroPie/retropiemenu/dispMenu.sh
-cp dispMenuCM3.sh /home/pi/RetroPie/retropiemenu/dispMenu.sh
-sudo rm /home/pi/RetroPie/retropiemenu/icons/display.png
-cp display.png /home/pi/RetroPie/retropiemenu/icons
-sudo cp fbcpCropped.service /lib/systemd/system/fbcpCropped.service
-sudo cp fbcpFilled.service /lib/systemd/system/fbcpFilled.service
+INSTALL_DIR=/home/pi/Freeplay/$( ls /home/pi/Freeplay | grep -i freeplayili9341 )
+
+git -C $INSTALL_DIR pull
+
+sudo cp $INSTALL_DIR/fbcpFilled /usr/local/bin/fbcpFilled
+sudo cp $INSTALL_DIR/fbcpCropped /usr/local/bin/fbcpCropped
+cp $INSTALL_DIR/dispMenuCM3.sh /home/pi/RetroPie/retropiemenu/dispMenu.sh
+cp $INSTALL_DIR/display.png /home/pi/RetroPie/retropiemenu/icons
+sudo cp $INSTALL_DIR/fbcpCropped.service /lib/systemd/system/fbcpCropped.service
+sudo cp $INSTALL_DIR/fbcpFilled.service /lib/systemd/system/fbcpFilled.service
 
 sudo systemctl disable fbcpFilled.service
 sudo systemctl disable fbcpCropped.service
 sudo update-rc.d fbcp.sh enable
 
-sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./dispMenu.sh</path>\n\t\t<name>FreePlay Change Display Driver</name>\n\t\t<desc>Choose which display driver to use, between two versions of the experimental driver and the default driver</desc>\n\t\t<image>./icons/display.png</image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+if grep -q "Freeplay Change Display Driver" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
+	echo "Display Driver Menu Item Already Added"
+else
+	sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./dispMenu.sh</path>\n\t\t<name>Freeplay Change Display Driver</name>\n\t\t<desc>Choose which display driver to use, between two versions of the experimental driver and the default driver</desc>\n\t\t<image>./icons/display.png</image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+fi
--- setupZero.sh
+++ setupZero.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-sudo cp fbcpZero /usr/local/bin
-sudo rm /home/pi/RetroPie/retropiemenu/dispMenu.sh
-sudo cp fbcpZeroNoDMA /usr/local/bin
-sudo rm /home/pi/RetroPie/retropiemenu/dispMenu.sh
-cp dispMenuZero.sh /home/pi/RetroPie/retropiemenu/dispMenu.sh
-sudo rm /home/pi/RetroPie/retropiemenu/icons/display.png
-cp display.png /home/pi/RetroPie/retropiemenu/icons
-sudo cp fbcpZero.service /lib/systemd/system/fbcpZero.service
+INSTALL_DIR=/home/pi/Freeplay/$( ls /home/pi/Freeplay | grep -i freeplayili9341 )
+
+git -C $INSTALL_DIR pull
+
+sudo cp $INSTALL_DIR/fbcpZero /usr/local/bin/fbcpZero
+sudo cp $INSTALL_DIR/fbcpZeroNoDMA /usr/local/bin/fbcpZeroNoDMA
+cp $INSTALL_DIR/dispMenuZero.sh /home/pi/RetroPie/retropiemenu/dispMenu.sh
+cp $INSTALL_DIR/display.png /home/pi/RetroPie/retropiemenu/icons
+sudo cp $INSTALL_DIR/fbcpZero.service /lib/systemd/system/fbcpZero.service
 
 sudo systemctl disable fbcpZero.service
 sudo systemctl disable fbcpZeroNoDMA.service
 sudo update-rc.d fbcp.sh enable
 
-sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./dispMenu.sh</path>\n\t\t<name>FreePlay Change Display Driver</name>\n\t\t<desc>Choose which display driver to use, between two versions of the experimental driver and the default driver</desc>\n\t\t<image>./icons/display.png</image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+if grep -q "Freeplay Change Display Driver" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
+	echo "Display Driver Menu Item Already Added"
+else
+	sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./dispMenu.sh</path>\n\t\t<name>Freeplay Change Display Driver</name>\n\t\t<desc>Choose which display driver to use, between two versions of the experimental driver and the default driver</desc>\n\t\t<image>./icons/display.png</image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed>20180514T205700</lastplayed>\n\t</game>\n</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+fi
