72fb685931b24b6e77f6d258d5de5b7de36c9691
Matthew Krohn <matthew.d.krohn@gmail.com>
2019-08-20 10:03:18 -0600

    Moved to new CM3 menu

--- dispMenuCM3.sh
+++ dispMenuCM3.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-INPUT=/tmp/menu.sh.$$
+#Most of new menu taken from https://askubuntu.com/questions/901247/bash-dialog-menu-leaves-menu
 
 stop_all_running_services ()
 {
@@ -42,7 +42,7 @@ use_dev_cropped ()
 {
 	sudo sed -i "s|^dtoverlay=waveshare32b|#FP#dtoverlay=waveshare32b|" /boot/config.txt
 
-	stop_all_running_services
+		stop_all_running_services
 
 	sleep 1
 
@@ -57,7 +57,7 @@ use_dev_filled ()
 {
 	sudo sed -i "s|^dtoverlay=waveshare32b|#FP#dtoverlay=waveshare32b|" /boot/config.txt
 
-	stop_all_running_services
+		stop_all_running_services
 
 	sleep 1
 
@@ -72,7 +72,7 @@ use_bp_cropped ()
 {
 	sudo sed -i "s|^dtoverlay=waveshare32b|#FP#dtoverlay=waveshare32b|" /boot/config.txt
 
-	stop_all_running_services
+		stop_all_running_services
 
 	sleep 1
 
@@ -83,11 +83,11 @@ use_bp_cropped ()
 	sudo reboot
 }
 
-use_dev_cropped_sleep ()
+use_dev_cropped_nosleep ()
 {
 	sudo sed -i "s|^dtoverlay=waveshare32b|#FP#dtoverlay=waveshare32b|" /boot/config.txt
 
-	stop_all_running_services
+		stop_all_running_services
 
 	sleep 1
 
@@ -103,7 +103,7 @@ use_bp_cropped_sleep ()
 {
 	sudo sed -i "s|^dtoverlay=waveshare32b|#FP#dtoverlay=waveshare32b|" /boot/config.txt
 
-	stop_all_running_services
+		stop_all_running_services
 
 	sleep 1
 
@@ -114,11 +114,11 @@ use_bp_cropped_sleep ()
 	sudo reboot
 }
 
-use_dev_filled_sleep ()
+use_dev_filled_nosleep ()
 {
 	sudo sed -i "s|^dtoverlay=waveshare32b|#FP#dtoverlay=waveshare32b|" /boot/config.txt
 
-	stop_all_running_services
+		stop_all_running_services
 
 	sleep 1
 
@@ -144,30 +144,105 @@ use_std ()
 	sudo reboot
 }
 
-dialog --clear --title "LCD Driver Selection" \
-	--menu "Choose which LCD Driver you would like to use" 15 50 10 \
-	Stable "Stable, slow driver" \
-	FastCropped "Cropped for the GBA viewport" \
-	FastFilled "Fills the entire display" \
-	FastNoSleepCropped "Cropped, no sleep" \
-	FastNoSleepFilled "Fills, no sleep" \
-	FastBPCrop "Boxy Pixel Cropped" \
-	FastBPNoSleepCrop "BP Cropped, No Sleep" \
-	Update "Update binaries and Menu" \
-	Exit "Exit without any changes" 2>"${INPUT}"
+OPTIONS=(Stable "Stable, low framerate"
+FastCropped "Cropped for the GBA viewport"
+FastFilled "Fills the entire display"
+FastNoSleepCropped "Cropped, no sleep"
+FastNoSleepFilled "Fills, no sleep"
+FastBPCrop "Boxy Pixel Cropped"
+FastBPNoSleepCrop "BP Cropped, No Sleep"
+Update "Update binaries and Menu"
+Exit "Exit without any changes")
 
-menuitem=$(<"${INPUT}")
+CMD=(dialog --clear --title "LCD Driver Selection" --menu "Choose which LCD Driver you would like to use" 15 50 15)
 
-case "$menuitem" in
-	Stable) use_std;;
-	FastCropped) use_dev_cropped;;
-	FastFilled) use_dev_filled;;
-	FastNoSleepCropped) use_dev_cropped_sleep;;
-	FastNoSleepFilled) use_dev_filled_sleep;;
-	FastBPCrop) use_bp_cropped;;
-	FastBPNoSleepCrop) use_bp_cropped_sleep;;
-	Update) update;;
-	Exit) echo "No changes made"; break;;
-esac
+while true; do
+	choices=$("${CMD[@]}" "${OPTIONS[@]}" 2>&1 >/dev/tty)
 
-[ -f "$INPUT" ] && rm "$INPUT"
+#If cancelled, drop the dialog
+if [ $? -ne 0  ]; then
+	clear;
+	exit;
+fi;
+
+for choice in $choices; do
+	case "$choice" in
+		Stable)
+			dialog --title "Stable Driver Description" --yesno "The stable driver is more thoroughly tested, but is less efficient and may have more frame drops. It also has customizable cropping by editing /boot/freeplayfbcp.cfg. Would you like to enable it?" 15 60
+			RESP=$?
+			case $RESP in
+				0) dialog --title "Stable Driver Selection" --infobox "Switching to Stable driver. System will turn off" 5 60;use_std;;
+				1) dialog --title "Stable Driver Selection" --infobox "NOT switching to Stable driver." 5 60;;
+				255) dialog --title "Stable Driver Selection" --infobox "Returning to main menu" 5 60;;
+			esac
+			;;
+		FastCropped)
+			dialog --title "Fast Cropped Driver Description" --yesno "The Fast Cropped driver drops few frames and has the necessary cropping for standard Freeplay units; it also has sleep enabled for battery saving, which may cause low framerates after periods of inactivity. Would you like to enable it?" 15 60
+			RESP=$?
+			case $RESP in
+				0) dialog --title "Fast Cropped Driver Selection" --infobox "Switching to Fast Cropped driver. System will turn off" 5 60; use_dev_cropped;;
+				1) dialog --title "Fast Cropped Driver Selection" --infobox "NOT switching to Fast Cropped driver." 5 60;;
+				255) dialog --title "Fast Cropped Driver Selection" --infobox "Returning to main menu" 5 60;;
+			esac
+			;;
+		FastFilled)
+			dialog --title "Fast Filled Driver Description" --yesno "The Fast Filled driver drops few frames and fills the display on large screen shells like the BoxyPixel; it also has sleep enabled for battery saving, which may cause low framerates after periods of inactivity. Would you like to enable it?" 15 60
+			RESP=$?
+			case $RESP in
+				0) dialog --title "Fast Filled Driver Selection" --infobox "Switching to Fast Filled driver. System will turn off" 5 60; use_dev_filled;;
+				1) dialog --title "Fast Filled Driver Selection" --infobox "NOT switching to Fast Filled driver." 5 60;;
+				255) dialog --title "Fast Filled Driver Selection" --infobox "Returning to main menu" 5 60;;
+			esac
+			;;
+		FastNoSleepCropped)
+			dialog --title "Fast No Sleep Cropped Driver Description" --yesno "The Fast No Sleep Cropped driver drops few frames and has the necessary cropping for standard Freeplay units; it also has sleep disabled which prevents framerate drops after periods of inactivity, but may reduce battery life. Would you like to enable it?" 15 60
+			RESP=$?
+			case $RESP in
+				0) dialog --title "Fast No Sleep Cropped Driver Selection" --infobox "Switching to Fast No Sleep Cropped driver. System will turn off" 5 60; use_dev_cropped_nosleep;;
+				1) dialog --title "Fast No Sleep Cropped Driver Selection" --infobox "NOT switching to Fast No Sleep Cropped driver." 5 60;;
+				255) dialog --title "Fast No Sleep Cropped Driver Selection" --infobox "Returning to main menu" 5 60;;
+			esac
+			;;
+		FastNoSleepFilled)
+			dialog --title "Fast No Sleep Filled Driver Description" --yesno "The Fast No Sleep Filled driver drops few frames and fills the display on large screen shells like the BoxyPixel; it also has sleep disabled which prevents framerate drops after periods of inactivity, but may reduce battery life. Would you like to enable it?" 15 60
+			RESP=$?
+			case $RESP in
+				0) dialog --title "Fast No Sleep Filled Driver Selection" --infobox "Switching to Fast No Sleep Filled driver. System will turn off" 5 60; use_dev_filled_nosleep;;
+				1) dialog --title "Fast No Sleep Filled Driver Selection" --infobox "NOT switching to Fast No Sleep Filled driver." 5 60;;
+				255) dialog --title "Fast No Sleep Filled Driver Selection" --infobox "Returning to main menu" 5 60;;
+			esac
+			;;
+		FastBPCrop)
+			dialog --title "Fast BoxyPixel Cropped Driver Description" --yesno "The Fast BoxyPixel Cropped driver drops few frames and the necessary cropping for a BoxyPixel shell using a Freeplay lens; it also has sleep enabled to save battery, but may cause framerate drops after periods of inactivity. Would you like to enable it?" 15 60
+			RESP=$?
+			case $RESP in
+				0) dialog --title "Fast BoxyPixel Cropped Driver Selection" --infobox "Switching to Fast BoxyPixel Cropped driver. System will turn off" 5 60; use_bp_cropped;;
+				1) dialog --title "Fast BoxyPixel Cropped Driver Selection" --infobox "NOT switching to Fast BoxyPixel Cropped driver." 5 60;;
+				255) dialog --title "Fast BoxyPixel Cropped Driver Selection" --infobox "Returning to main menu" 5 60;;
+			esac
+			;;
+		FastBPNoSleepCrop)
+			dialog --title "Fast No Sleep BoxyPixel Uncropped Driver Description" --yesno "The Fast BoxyPixel Cropped driver drops few frames and the necessary cropping for a BoxyPixel shell using a Freeplay lens; it also has sleep disabled to prevent framerate drops after periods of inactivity, but may reduce battery life. Would you like to enable it?" 15 60
+			RESP=$?
+			case $RESP in
+				0) dialog --title "Fast No Sleep BoxyPixel Uncropped Driver Selection" --infobox "Switching to Fast No Sleep BoxyPixel Uncropped driver. System will turn off" 5 60; use_bp_cropped_sleep;;
+				1) dialog --title "Fast No Sleep BoxyPixel Uncropped Driver Selection" --infobox "NOT switching to Fast No Sleep BoxyPixel Uncropped driver." 5 60;;
+				255) dialog --title "Fast No Sleep BoxyPixel Uncropped Driver Selection" --infobox "Returning to main menu" 5 60;;
+			esac
+			;;
+		Update)
+			dialog --title "Update Display Drivers" --yesno "Updating your drivers and menu items may take some time and requires an internet connection. Would you like to continue?" 15 60
+			RESP=$?
+			case $RESP in
+				0) dialog --title "Update Display Drivers" --infobox "Updating Display Drivers" 5 60; update;;
+				1) dialog --title "Update Display Drivers" --infobox "NOT Updating Display Drivers" 5 60;;
+				255) dialog --title "Update Display Drivers" --infobox "Returning to main menu" 5 60;;
+			esac
+			;;
+		Exit)
+			dialog --title "Exiting Display Driver Dialogs" --infobox "Returning to RetroPie Menu" 15 60; sleep 2;
+			exit
+			;;
+	esac
+done
+done
